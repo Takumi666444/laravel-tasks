d6a809a9375c84304472539bd7b9ae8c47634680
Takumi666444 <t.suzuki@kansaihw.jp>
2024-09-17 13:33:39 +0900

    Update database.php

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('AZURE_MYSQL_HOST', 'kansaihw-mysql.mysql.database.azure.com'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('AZURE_MYSQL_DBNAME', 'laravel-sample'),
             'username' => env('DB_USERNAME', 'forge'),
